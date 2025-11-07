03469d9c5bf557e2a72e3c837152fdd73ad2575f
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-07 09:45:28 +0700

    formisian

--- app/src/main/java/com/example/project6/view/Formulir.kt
+++ app/src/main/java/com/example/project6/view/Formulir.kt
@@ -2,3 +2,6 @@ package com.example.project6.view
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
