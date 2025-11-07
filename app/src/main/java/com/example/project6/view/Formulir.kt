47c669407605985f8e425e4a5cdd00b78dce1db0
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-07 16:34:49 +0700

    mengsolve eror

--- app/src/main/java/com/example/project6/view/Formulir.kt
+++ app/src/main/java/com/example/project6/view/Formulir.kt
@@ -3,13 +3,17 @@ package com.example.project6.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.R
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -21,56 +25,60 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.project6.R
 
 
-@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick: () -> Unit
-) {
-
-    Scaffold (modifier= Modifier,
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold(
+        modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.nama), color = Color.White) },
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = colorResource(id = R.color.teal_700)
                 )
             )
         }
     ) { isiRuang ->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             OutlinedTextField(
                 value = "",
                 singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(width = 250.dp),
-                label = {Text(text = "Nama Lengkap")},
+                label = { Text(text = "Nama Lengkap") },
                 onValueChange = {},
             )
-            HorizontalDivider(modifier = Modifier
-                .padding(all = 20.dp)
-                .width(width = 250.dp), thickness = Thickness, color = Color.Red)
-            Row{
-                jenisK.forEach {
-                        item->
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp), thickness = Thickness, color = Color.Red
+            )
+            Row {
+                jenisK.forEach { item ->
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
                             selected = false,
-                            onClick = {item}
+                            onClick = { item }
                         )
                         Text(text = item)
                     }
                 }
             }
-            HorizontalDivider(modifier = Modifier
-                .padding(all = 20.dp)
-                .width(width = 250.dp),
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp),
                 thickness = 1.dp,
                 color = Color.Red
             )
@@ -79,7 +87,7 @@ fun FormIsian(
                 singleLine = true,
                 modifier = Modifier
                     .width(width = 250.dp),
-                label = {Text(text = "Alamat")},
+                label = { Text(text = "Alamat") },
                 onValueChange = {},
             )
 
@@ -88,8 +96,10 @@ fun FormIsian(
             Button(
                 modifier = Modifier.fillMaxWidth(fraction = 1f)
                     .padding(all = 25.dp),
-                onClick = onSubmitBtnClick
+                onClick = OnSubmitBtnClick
             ) {
                 Text(text = stringResource(id = R.string.submit))
             }
         }
+    }
+}
