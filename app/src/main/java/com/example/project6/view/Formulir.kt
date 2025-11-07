aafe1143b7688e24ab3706eed61522ee2331884f
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-07 09:57:48 +0700

    add dependensi lagi

--- app/src/main/java/com/example/project6/view/Formulir.kt
+++ app/src/main/java/com/example/project6/view/Formulir.kt
@@ -1,13 +1,27 @@
 package com.example.project6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
