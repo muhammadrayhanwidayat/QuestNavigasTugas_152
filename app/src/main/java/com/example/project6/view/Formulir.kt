73bc83606dc027d578a17f789c458cdf9fe08c67
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-07 09:55:31 +0700

    add dependensi

--- app/src/main/java/com/example/project6/view/Formulir.kt
+++ app/src/main/java/com/example/project6/view/Formulir.kt
@@ -1,7 +1,13 @@
 package com.example.project6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -10,7 +16,7 @@ fun FormIsian(
     OnSubmitBtnClick: () -> Unit
 ) {
 
-    Scaffold (modifier=Modifier,
+    Scaffold (modifier= Modifier,
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.nama), color = Color.White) },
